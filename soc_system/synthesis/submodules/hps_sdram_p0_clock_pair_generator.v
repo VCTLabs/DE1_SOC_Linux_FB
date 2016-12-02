6773560e9ec2c7956a554cc3ad2bdefb0a3ed992
Steve Arnold <stephen.arnold42@gmail.com>
2016-12-02 14:00:21 -0800

    16.1 qsys-generate rebuild, no manual changes

--- soc_system/synthesis/submodules/hps_sdram_p0_clock_pair_generator.v
+++ soc_system/synthesis/submodules/hps_sdram_p0_clock_pair_generator.v
@@ -1,22 +1,22 @@
 //altiobuf_out CBX_AUTO_BLACKBOX="ALL" CBX_SINGLE_OUTPUT_FILE="ON" DEVICE_FAMILY="Cyclone V" ENABLE_BUS_HOLD="FALSE" NUMBER_OF_CHANNELS=1 OPEN_DRAIN_OUTPUT="FALSE" PSEUDO_DIFFERENTIAL_MODE="TRUE" USE_DIFFERENTIAL_MODE="TRUE" USE_OE="FALSE" USE_OUT_DYNAMIC_DELAY_CHAIN1="FALSE" USE_OUT_DYNAMIC_DELAY_CHAIN2="FALSE" USE_TERMINATION_CONTROL="FALSE" datain dataout dataout_b
-//VERSION_BEGIN 16.0 cbx_altiobuf_out 2016:04:27:18:05:34:SJ cbx_mgl 2016:04:27:18:06:48:SJ cbx_stratixiii 2016:04:27:18:05:34:SJ cbx_stratixv 2016:04:27:18:05:34:SJ  VERSION_END
+//VERSION_BEGIN 16.1 cbx_altiobuf_out 2016:10:19:21:26:20:SJ cbx_mgl 2016:10:19:22:10:30:SJ cbx_stratixiii 2016:10:19:21:26:20:SJ cbx_stratixv 2016:10:19:21:26:20:SJ  VERSION_END
 // synthesis VERILOG_INPUT_VERSION VERILOG_2001
 // altera message_off 10463
 
 
 
-// Copyright (C) 1991-2016 Altera Corporation. All rights reserved.
-//  Your use of Altera Corporation's design tools, logic functions 
+// Copyright (C) 2016  Intel Corporation. All rights reserved.
+//  Your use of Intel Corporation's design tools, logic functions 
 //  and other software and tools, and its AMPP partner logic 
 //  functions, and any output files from any of the foregoing 
 //  (including device programming or simulation files), and any 
 //  associated documentation or information are expressly subject 
-//  to the terms and conditions of the Altera Program License 
-//  Subscription Agreement, the Altera Quartus Prime License Agreement,
-//  the Altera MegaCore Function License Agreement, or other 
+//  to the terms and conditions of the Intel Program License 
+//  Subscription Agreement, the Intel Quartus Prime License Agreement,
+//  the Intel MegaCore Function License Agreement, or other 
 //  applicable license agreement, including, without limitation, 
 //  that your use is for the sole purpose of programming logic 
-//  devices manufactured by Altera and sold by Altera or its 
+//  devices manufactured by Intel and sold by Intel or its 
 //  authorized distributors.  Please refer to the applicable 
 //  agreement for further details.
 
